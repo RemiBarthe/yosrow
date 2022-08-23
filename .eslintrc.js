21794ae33303c003cfa5918784bad2a1f768363e
RemiBarthe <barthe.rem@gmail.com>
2022-08-23 23:07:42 +0200

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-vars': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'template-curly-spacing': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': ['error', { int32Hint: true }],
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    indent: ['error', 2],
+    'key-spacing': ['error', { afterColon: true, beforeColon: false }]
+  }
 };
