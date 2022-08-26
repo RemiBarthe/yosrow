a9a747bc7853bf67f28e43ca320d2713ff81a945
RemiBarthe <barthe.rem@gmail.com>
2022-08-27 01:22:39 +0200

    Cleaning code

---
+++ src/containers/TimerContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+    <p>{{ timer }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TimerContainer',
+  data: () => ({
+    timer: 0
+  }),
+  mounted() {
+    setInterval(() => {
+      this.timer++;
+    }, 1000);
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
